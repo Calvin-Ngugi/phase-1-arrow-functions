a6148c49f9c62b6d839eba9734bb101fa22590f0
Calvin <ncalvin67@gmail.com>
2022-08-19 10:11:21 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide(num1, num2){
+    num1 = 2000;
+    num2 = 100;
+    return num1/num2;
+}
+
+const square = (par) =>{
+    return par * par
+}
+
+const add = (x, y) => {
+    return x + y;
+}
